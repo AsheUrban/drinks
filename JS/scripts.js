cdec4c18ff90e1362d9b357fcf626004c79b11dc
Ashe Urban <ashe@goldentongue.com>
2022-06-02 16:09:04 -0700

    Add message and function for if esle user === 21.
    Co-authored-by: Derrak Richard <derrak@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
   const age = parseInt(prompt("How old are you?"));
 
-  if (age >= 21) {
+  if (age > 21) {
     $('#drinks').show();
   } 
-  else {
+    else if (age === 21) {
+    alert("Now don't go wild!");
+    $('#drinks').show();
+  }
+    else {
     $('#under-21').show();
   }
 
+  
 });
