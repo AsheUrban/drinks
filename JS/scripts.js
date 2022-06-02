4af1840de5075d0772969e6845e2c55e430ea6f2
Ashe Urban <ashe@goldentongue.com>
2022-06-02 16:17:36 -0700

    Add function for over21 = confirm dialogue box and comment out previous function.
    Co-authored-by: Derrak Richard <derrak@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
-  const age = parseInt(prompt("How old are you?"));
+  const over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
 
-  if (age > 21) {
+  if (over21) {
     $('#drinks').show();
-  } 
-    else if (age === 21) {
-    alert("Now don't go wild!");
-    $('#drinks').show();
-  }
-    else {
+  } else {
     $('#under-21').show();
   }
-
-  
 });
+
+// $(document).ready(function() {
+//   const age = parseInt(prompt("How old are you?"));
+
+//   if (age > 21) {
+//     $('#drinks').show();
+//   } 
+//     else if (age === 21) {
+//     alert("Now don't go wild!");
+//     $('#drinks').show();
+//   }
+//     else {
+//     $('#under-21').show();
+//   }
+
+// });
